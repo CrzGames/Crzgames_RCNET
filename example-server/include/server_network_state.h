85b9157c9e0131ef87460f79ba07a9d1dc93020e
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-08 00:45:55 +0100

    m

--- example-server/include/server_network_state.h
+++ example-server/include/server_network_state.h
@@ -16,22 +16,32 @@ struct NetworkState
     // ------------------------------------------------------------------------
 
     // Port d'écoute du serveur
-    const uint16_t serverPort = 12345;
+    static constexpr uint16_t serverPort = 12345;
 
-    // Nombre de channels ENet (ex: handshake, reliable, unreliable)
-    const uint8_t channelCount = static_cast<uint8_t>(NetworkChannel::COUNT);
+    // Nombre de channels ENet utilisés
+    static constexpr uint8_t channelCount = static_cast<uint8_t>(NetworkChannel::COUNT);
 
     // Nombre maximum de clients connectés
-    const uint32_t maxClientsConnected = 2;
+    static constexpr uint32_t maxClientsConnected = 2;
 
     // Fréquence de tick de simulation du serveur en Hz (ex: 128)
-    uint32_t simulationTickRateHz = 128;
+    static constexpr uint32_t simulationTickRateHz = 128;
 
     // Fréquence de tick réseau OUT du serveur en Hz (ex: 32)
-    uint32_t networkOutgoingTickRateHz = 32;
+    static constexpr uint32_t networkOutgoingTickRateHz = 32;
 
     // Durée de sommeil entre chaque tick réseau IN en ms (ex: 1)
-    uint32_t networkIncomingSleepMs = 1;
+    static constexpr uint32_t networkIncomingSleepMs = 1;
+
+#if SERVER_ENV_DEV
+    static constexpr std::string baseUrlApi = "http://localhost:3400";
+#elif SERVER_ENV_STAGING
+    static constexpr std::string baseUrlApi = "https://staging.api.monsite.com";
+#elif SERVER_ENV_PRODUCTION
+    static constexpr std::string baseUrlApi = "https://api.monsite.com";
+#else
+#   error "Environnement serveur inconnu"
+#endif
 
 
     // ------------------------------------------------------------------------
