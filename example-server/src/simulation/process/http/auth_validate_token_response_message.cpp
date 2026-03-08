4589e2b78156207633458123f8f4f0d81fb30e89
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-08 23:56:49 +0100

    m

--- example-server/src/simulation/process/http/auth_validate_token_response_message.cpp
+++ example-server/src/simulation/process/http/auth_validate_token_response_message.cpp
@@ -1,4 +1,4 @@
-#include "simulation/process/http/auth_validate_token_response.h"
+#include "simulation/process/http/auth_validate_token_response_message.h"
 
 #include "auth/types.h"
 #include "network/packets/server/reliable.h"
