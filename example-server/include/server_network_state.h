1dca87f5d4cc4c1986fb632b2e1e939819351183
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-08 01:06:56 +0100

    m

--- example-server/include/server_network_state.h
+++ example-server/include/server_network_state.h
@@ -39,8 +39,6 @@ struct NetworkState
     static constexpr std::string baseUrlApi = "https://staging.api.aetherroyale.crzgames.com";
 #elif SERVER_ENV_PRODUCTION
     static constexpr std::string baseUrlApi = "https://api.aetherroyale.crzgames.com";
-#else
-#   error "Environnement serveur inconnu"
 #endif
 
 
