a8910b205fae8e82990511fa97ffc603770a08d5
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-08 22:56:25 +0100

    m

--- example-server/include/simulation/snapshots.h
+++ example-server/include/simulation/snapshots.h
@@ -7,18 +7,6 @@
 #include "core/threading/queues/simulation_to_network_outgoing.h"
 
 /**
- * @brief Construit un snapshot full pour une session puis l'ajoute à la queue réseau sortante.
- *
- * @param simToNetQueue Queue simulation -> réseau utilisée pour l'envoi des snapshots.
- * @param session Session destinataire du snapshot.
- * @param currentTick Tick courant de simulation.
- */
-void ServerSimulationUpdate_CreateFullSnapshotAndEnqueue(
-    SimulationToNetworkOUTQueue& simToNetQueue,
-    ClientSession& session,
-    uint64_t currentTick);
-
-/**
  * @brief Construit les snapshots pour toutes les sessions puis les ajoute à la queue réseau sortante.
  *
  * @param simToNetQueue Queue simulation -> réseau utilisée pour l'envoi des snapshots.
