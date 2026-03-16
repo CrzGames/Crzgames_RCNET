9389296d0f376b12cf7dba7dfa57f723da3c6106
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-16 15:37:44 +0100

    x

--- example-client/src/game.cpp
+++ example-client/src/game.cpp
@@ -1,194 +1,83 @@
 #include <game.h>
 #include <game_screen.h>
 
-#include <RC2D/RC2D.h>
-
-#include <rcenet/RCENET_enet.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 GameScreen gameScreen;
-
-static ENetHost* enetClientHost = NULL;
-static ENetPeer* enetServerPeer = NULL;
-static bool isConnectedToServer = false;
-
-static void net_disconnect_and_destroy(void)
-{
-    if (!enetClientHost) return;
-
-    if (enetServerPeer)
-    {
-        // Demande de disconnect propre
-        enet_peer_disconnect(enetServerPeer, 0);
-
-        // On pompe un peu pour recevoir DISCONNECT
-        ENetEvent event;
-        while (enet_host_service(enetClientHost, &event, 3000) > 0)
-        {
-            if (event.type == ENET_EVENT_TYPE_RECEIVE)
-                enet_packet_destroy(event.packet);
-            else if (event.type == ENET_EVENT_TYPE_DISCONNECT)
-                break;
-        }
-
-        // Au cas où
-        enet_peer_reset(enetServerPeer);
-        enetServerPeer = NULL;
-    }
-
-    enet_host_destroy(enetClientHost);
-    enetClientHost = NULL;
-}
-
-static void net_connect_to_server()
-{
-    // ------------------------------------------------------------
-    // Serveur ENet : se connecter à localhost:12345
-    // ------------------------------------------------------------
-    const char* serverHost = "127.0.0.1";
-    uint16_t serverPort = 12345;
-
-    // ------------------------------------------------------------
-    // 1) Résoudre adresse serveur
-    // ------------------------------------------------------------
-    ENetAddress serverAddress;
-    memset(&serverAddress, 0, sizeof(serverAddress));
-
-    enet_address_set_host(&serverAddress, ENET_ADDRESS_TYPE_ANY, serverHost);
-    serverAddress.port = serverPort;
-
-    // ------------------------------------------------------------
-    // 2) Créer le host client
-    // channels = 4 (comme le serveur)
-    // ------------------------------------------------------------
-    enetClientHost = enet_host_create(
-        serverAddress.type,   // IPv4/IPv6 selon ce qui a été résolu
-        NULL,                 // NULL => client host
-        1,                    // 1 connexion sortante max
-        4,                    // channels
-        0,                    // incoming bandwidth
-        0                     // outgoing bandwidth
-    );
-    if (!enetClientHost)
-    {
-        RC2D_log(RC2D_LOG_INFO, "ENet: échec de la création du host client.\n");
-        net_disconnect_and_destroy();
-        exit(EXIT_FAILURE);
-    }
-
-    // ------------------------------------------------------------
-    // 3) Connect au serveur ENet : localhost:12345
-    // ------------------------------------------------------------
-    enetServerPeer = enet_host_connect(enetClientHost, &serverAddress, 4 /* channels */, 0);
-    if (!enetServerPeer)
-    {
-        RC2D_log(RC2D_LOG_INFO, "ENet: échec de la connexion au serveur.\n");
-        net_disconnect_and_destroy();
-        exit(EXIT_FAILURE);
-    }
-
-    // ------------------------------------------------------------
-    // 4) Attendre l'event CONNECT (max 5 secondes)
-    // ------------------------------------------------------------
-    ENetEvent event;
-    if (enet_host_service(enetClientHost, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT)
-    {
-        RC2D_log(RC2D_LOG_INFO, "ENet: connexion au serveur réussie.\n");
-        isConnectedToServer = true;
-    }
-    else
-    {
-        net_disconnect_and_destroy();
-        RC2D_log(RC2D_LOG_INFO, "ENet: connexion au serveur FAIL.\n");
-    }
-}
-
-static void net_pump_events(void)
-{
-    if (!enetClientHost) return;
-
-    // ------------------------------------------------------------
-    // Boucle client simple :
-    // - service events (receive/disconnect)
-    // - envoyer input JSON toutes les ~16ms (≈60Hz)
-    // ------------------------------------------------------------
-    uint32_t clientTickId = 0;
-    uint32_t inputSequenceNumber = 0;
-
-    // 0 ms => non bloquant, parfait pour un loop jeu
-    ENetEvent event;
-    while (enet_host_service(enetClientHost, &event, 0) > 0)
-    {
-        switch (event.type)
-        {
-            case ENET_EVENT_TYPE_CONNECT:
-                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [CONNECT] - Connected to server.\n");
-                break;
-
-            case ENET_EVENT_TYPE_RECEIVE:
-                if(event.channelID == 0) // channel SECURE_SESSION_RELIABLE (0)
-                {
-                    // Traiter le message de handshake du serveur (ex: token, accountIdDatabase, etc.)
-                    RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [SECURE_SESSION] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
-                }
-                else if (event.channelID == 1) // channel AUTH_RELIABLE (1)
-                {
-                    RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [AUTH] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
-                }
-                else if (event.channelID == 2) // channel GAME RELIABLE (2)
-                {
-                    RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [GAME_RELIABLE] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
-                }
-                else if (event.channelID == 3) // channel GAME UNRELIABLE (3)
-                {
-                    RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [GAME_UNRELIABLE] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
-                }
-
-                // IMPORTANT: détruire le packet après usage
-                if (event.packet != nullptr)
-                {
-                    enet_packet_destroy(event.packet);
-                    event.packet = nullptr;
-                }
-                break;
-
-            // Le serveur déconnecte le client
-            case ENET_EVENT_TYPE_DISCONNECT:
-                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [DISCONNECT] - Disconnected from server.\n");
-                break;
-
-            // Timeout de connexion au serveur (ex: serveur éteint, crash du serveur, etc.)
-            case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT:
-                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [DISCONNECT_TIMEOUT] - Server connection timeout.\n");
-                enetServerPeer = NULL;
-                isConnectedToServer = false;
-                break;
-
-            default:
-                break;
-        }
-    }
-}
-
-/* -------------------- RC2D lifecycle -------------------- */
 
 void rc2d_unload(void)
 {
-    net_disconnect_and_destroy();
+
 }
 
 void rc2d_load(void)
 {
-    net_connect_to_server();
+    //rc2d_engine_networkConnectToServer("127.0.0.1", 12345);
 }
 
 void rc2d_update(double dt)
 {
-    net_pump_events();
     gameScreen.update(dt);
+}
+
+void rc2d_simulation_update(uint64_t currentTick, uint64_t dtNs, double dt)
+{
+
+}
+
+void rc2d_network_incoming_update(ENetHost* host, const ENetEvent* event)
+{
+    switch (event.type)
+    {
+        case ENET_EVENT_TYPE_CONNECT:
+            RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [CONNECT] - Connected to server.\n");
+            break;
+
+        case ENET_EVENT_TYPE_RECEIVE:
+            if(event.channelID == 0) // channel SECURE_SESSION_RELIABLE (0)
+            {
+                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [SECURE_SESSION] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
+            }
+            else if (event.channelID == 1) // channel AUTH_RELIABLE (1)
+            {
+                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [AUTH] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
+            }
+            else if (event.channelID == 2) // channel GAME RELIABLE (2)
+            {
+                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [GAME_RELIABLE] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
+            }
+            else if (event.channelID == 3) // channel GAME UNRELIABLE (3)
+            {
+                RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [GAME_UNRELIABLE] - Packet received from server (size=%u bytes)\n", (unsigned)event.packet->dataLength);
+            }
+            break;
+
+        // Le serveur déconnecte le client
+        case ENET_EVENT_TYPE_DISCONNECT:
+            RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [DISCONNECT] - Disconnected from server.\n");
+            break;
+
+        // Timeout de connexion au serveur (ex: serveur éteint, crash du serveur, etc.)
+        case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT:
+            RC2D_log(RC2D_LOG_INFO, "[CLIENT] [NETWORK_IN] [DISCONNECT_TIMEOUT] - Server connection timeout.\n");
+            break;
+
+        default:
+            break;
+    }
+}
+
+void rc2d_network_outgoing_update(ENetHost* host)
+{
+
+}
+
+void rc2d_http_update(void)
+{
+
+}
+
+void rc2d_websocket_update(void)
+{
+
 }
 
 void rc2d_draw(void)
--- example-client/src/main.cpp
+++ example-client/src/main.cpp
@@ -55,6 +55,13 @@ const RC2D_EngineConfig* rc2d_engine_setup(int argc, char* argv[])
     config->callbacks->rc2d_unload = rc2d_unload;
     config->callbacks->rc2d_mousepressed = rc2d_mousepressed;
     config->callbacks->rc2d_keypressed = rc2d_keypressed;
+    config->callbacks->rc2d_simulation_update = rc2d_simulation_update;
+    config->callbacks->rc2d_network_incoming_update = rc2d_network_incoming_update;
+    config->callbacks->rc2d_network_outgoing_update = rc2d_network_outgoing_update;
+    config->callbacks->rc2d_http_update = rc2d_http_update;
+    config->callbacks->rc2d_websocket_update = rc2d_websocket_update;
+    config->networkClientConfig->autoConnectOnStart = true;
+    config->networkClientConfig->autoReconnectOnDisconnect = true;
     config->logicalPresentationMode = RC2D_LOGICAL_PRESENTATION_OVERSCAN;
     config->pixelartMode = true;
     config->appInfo->name = "Aether Royale";
