4c797430e218af272d1d59780f621071dc10b95a
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-19 03:05:13 +0100

    m

--- example-server/include/core/callbacks.h
+++ example-server/include/core/callbacks.h
@@ -13,4 +13,5 @@ void rcnet_network_host_setup(ENetHost* host);
 void rcnet_network_outgoing_update(ENetHost* host);
 void rcnet_simulation_update(uint64_t currentTick, uint64_t serverTimeNs, uint64_t dtNs, double dt);
 void rcnet_http_update(void);
+void rcnet_nats_update(RCNET_NATSContext* natsContext);
 void rcnet_wake_blocking_threads(void);
