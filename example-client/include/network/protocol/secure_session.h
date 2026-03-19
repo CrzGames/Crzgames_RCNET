f49f03d40da5aaf162114a7fc6894096517029a9
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-19 02:13:08 +0100

    x

--- example-client/include/network/protocol/secure_session.h
+++ example-client/include/network/protocol/secure_session.h
@@ -22,19 +22,14 @@ static constexpr char CLIENT_SECURE_SESSION_SIGNING_DOMAIN[] =
     "SERVER_ED25519_DOMAIN_SECURE_SESSION_ATTESTATION_V1";
 
 // Cle publique Ed25519 pinnee cote client pour l'environnement DEV.
-// Cette valeur est derivee de la seed serveur (DEV) et sert a verifier
-// ServerSecureSessionHelloResponsePacketReliable::signature.
-// La seed privee serveur NE DOIT JAMAIS etre presente cote client.
 static constexpr char CLIENT_SECURE_SESSION_SERVER_ED25519_PUBLIC_KEY_HEX_DEV[] =
     "af110dde7833e9aa3784e6b44af5754a67b659a09dd85877bdc0330c79e7e02f";
 
 // Cle publique Ed25519 pinnee cote client pour l'environnement STAGING.
-// Remplacer par la cle publique reelle de l'environnement staging.
 static constexpr char CLIENT_SECURE_SESSION_SERVER_ED25519_PUBLIC_KEY_HEX_STAGING[] =
     "8dd7caa479c6f3ee450605e2bb0d47fdcef9687314c5101f1ad03db54f28ac0c";
 
 // Cle publique Ed25519 pinnee cote client pour l'environnement PRODUCTION.
-// Remplacer par la cle publique reelle de l'environnement production.
 static constexpr char CLIENT_SECURE_SESSION_SERVER_ED25519_PUBLIC_KEY_HEX_PRODUCTION[] =
     "8dd7caa479c6f3ee450605e2bb0d47fdcef9687314c5101f1ad03db54f28ac0c";
 
